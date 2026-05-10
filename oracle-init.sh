6a1a4bbe586444120a99ead5bb11c566e3fad230
Jan Grmela <41264018+grmelacz@users.noreply.github.com>
2026-05-10 19:14:09 +0200

    Base init script

---
+++ oracle-init.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -e          # Exit immediately if any command fails (recommended)
+set -x          # Optional: print every command as it runs (great for debugging)
+
+echo "=== Starting VM init on $(hostname) ==="
+
+# base
+sudo apt update
+sudo apt install -y htop vim fail2ban ufw rsync iputils-ping less ca-certificates curl
+
+# install docker
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# finish base
+sudo apt upgrade -y
+
+# config firewall
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow from 10.0.0.0/24
+sudo ufw allow ssh
+sudo ufw allow http
+sudo ufw allow https
+sudo ufw enable --force
+
+# config fail2ban
+sudo cp /etc/fail2ban/jail.d/defaults-debian.conf /etc/fail2ban/jail.d/defaults-debian.conf.bak.$(date +%Y%m%d-%H%M%S)
+sudo bash -c '
+  FILE="/etc/fail2ban/jail.d/defaults-debian.conf"
+  if ! grep -qE "^[[:space:]]*(bantime|maxretry)[[:space:]]*=[[:space:]]*" "$FILE"; then
+    echo -e "\nbantime = 24h\nmaxretry = 3" | tee -a "$FILE" > /dev/null
+    echo "Done: Added bantime and maxretry to [sshd] section."
+  else
+    echo "Error: Settings for bantime or maxretry already exist in the file (skipped)."
+  fi
+'
+
+echo "=== VM init successfull ==="
+
+exit 0
