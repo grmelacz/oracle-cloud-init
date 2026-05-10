713c0652f6a89a9ae13c7ebbda12f39a9a1c8593
Jan Grmela <41264018+grmelacz@users.noreply.github.com>
2026-05-10 19:39:11 +0200

    Update oracle-init.sh for improved installation
    APT needs to be updated after the new package source is added

--- oracle-init.sh
+++ oracle-init.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e          # Exit immediately if any command fails (recommended)
-set -x          # Optional: print every command as it runs (great for debugging)
-
 echo "=== Starting VM init on $(hostname) ==="
 
 # base
@@ -21,7 +19,8 @@ Architectures: $(dpkg --print-architecture)
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # finish base
 sudo apt upgrade -y
